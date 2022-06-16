ae74d3ac85ae6bb5ef5a94cd8a857b7ced7211d2
abe-manjaro-desktop <a.brandsma@protonmail.com>
2022-06-16 15:03:50 +0200

    LSTM works for localization, updated simulation to include multiple runs

--- code/1-data_creation/simulation/simulation.py
+++ code/1-data_creation/simulation/simulation.py
@@ -33,51 +33,63 @@ def v_y(s, x, y, theta, a, norm_w):
     return C * (wavelet_n(p) * math.sin(theta) -
                 wavelet_o(p) * math.cos(theta))
 
+
 def simulate_new(theta,
                  a,
                  norm_w,
-                 sensor_range=(0, 1024),
-                 x_range=(0, 1000),
-                 y_range=(0, 3),
+                 sensor_range=(0, 64),
+                 x_range=(-32, 32),
+                 y_range=(2, 3),
+                 number_of_x_steps=1024,
+                 number_of_runs=120,
                  sampling_rate=1,
                  folder_path="../../../data/"):
 
-    sensors = list(range(sensor_range[0], sensor_range[1] * sampling_rate))
     input_sensors = list(
+        np.arange(x_range[0], x_range[1], (x_range[1] - x_range[0]) /
+                  (sensor_range[1] * sampling_rate)))
+    x_input = list(
         np.arange(x_range[0], x_range[1],
-                  x_range[1] / (sensor_range[1] * sampling_rate)))
-    data = []
-    labels = []
-    
-    for sensor in tqdm(sensors):
-        data.append([])
-        labels.append([])
+                  (abs(x_range[0]) + abs(x_range[1])) / number_of_x_steps))
+    all_data = []
+    all_labels = []
+    for _ in tqdm(range(number_of_runs)):
+        data = []
+        labels = []
         for y in range(y_range[0], y_range[1]):
-            data[sensor].append([])
-            labels[sensor].append([])
-            for x in range(x_range[0], x_range[1]):
-                # input_sensor = (
-                #     sensor +
-                #     (x_range[1] /
-                #      (sensor_range[1] * sampling_rate))) / sampling_rate
-                input_sensor = input_sensors[sensor]
+            data.append([])
+            labels.append([])
+            for x_idx, x in enumerate(x_input):
+                data[y - y_range[0]].append([])
+                labels[y - y_range[0]].append([])
+                for input_sensor in input_sensors:
+                    # NOTE: the x and y coordinates are different than the array coordinates
+                    data[y - y_range[0]][x_idx].append(
+                        v_x(input_sensor, x, y + 1, theta, a, norm_w))
+                    data[y - y_range[0]][x_idx].append(
+                        v_y(input_sensor, x, y + 1, theta, a, norm_w))
 
-                # NOTE: the x and y coordinates (sensor also) are different than the array coordinates
-                data[sensor][y].append(
-                    (v_x(input_sensor, x + 1, y + 1, theta, a, norm_w),
-                     v_y(input_sensor, x + 1, y + 1, theta, a, norm_w)))
-                    
-                labels[sensor][y].append((x + 1, y + 1))
+                labels[y - y_range[0]][x_idx].append(x)
+                labels[y - y_range[0]][x_idx].append(y + 1)
+
+        all_data.append(data)
+        all_labels.append(labels)
 
     data_path = folder_path + f"a{a}_normw{norm_w}_theta{theta}.npy"
     labels_path = folder_path + f"a{a}_normw{norm_w}_theta{theta}_labels.npy"
 
-    data = np.array(data)
-    labels = np.array(labels)
-    print(data.shape)
-    print(labels.shape)
-    np.save(data_path, data)
-    np.save(labels_path, labels)
+    all_data = np.array(all_data)
+    all_labels = np.array(all_labels)
+    all_data = np.reshape(all_data, (all_data.shape[0], all_data.shape[1] *
+                                     all_data.shape[2], all_data.shape[3]))
+    all_labels = np.reshape(all_labels,
+                            (all_labels.shape[0], all_labels.shape[1] *
+                             all_labels.shape[2], all_labels.shape[3]))
+
+    print(all_data.shape)
+    print(all_labels.shape)
+    np.save(data_path, all_data)
+    np.save(labels_path, all_labels)
 
 
 def main():
