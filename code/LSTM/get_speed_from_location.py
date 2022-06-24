9987914af9428110596fcc4b4f30ca5fba95c722
abe-manjaro-desktop <a.brandsma@protonmail.com>
2022-06-24 18:03:00 +0200

    Runs to test speed extraction again, priming for volume extraction

--- code/LSTM/get_speed_from_location.py
+++ code/LSTM/get_speed_from_location.py
@@ -17,10 +17,10 @@ def read_inputs():
     stride = 2
     alpha = 0.05
     decay = 1e-9
-    shuffle_data = True
+    shuffle_data = False
     data_split = 0.8
     dropout = 0
-    train_loc = "../../data/a1_theta0.npy"
+    train_loc = "../../data/a10_theta0.npy"
     ac_fun = "relu"
     return n_nodes, n_epochs, window_size, stride, \
         alpha, decay, shuffle_data, data_split, dropout, train_loc, ac_fun
@@ -39,7 +39,7 @@ def main():
     data = Data(settings, train_location)
 
     new_model = tf.keras.models.load_model(
-        './trained_models/win16_stride2_epochs10_latest')
+        './trained_models/win16_stride2_epochs30_dropout0_latest')
     new_model.summary()
     prev_x = 0
     prev_time = 0
@@ -47,50 +47,56 @@ def main():
 
     speeds = []
     real_speeds = []
-    run_idx = 40
+    # run_idx = 8
 
-    for idx in tqdm(range(0, 1024, window_size)):
-        input_data = data.test_data[run_idx][idx:idx + window_size]
-        input_data = np.reshape(input_data, (1, window_size, 128))
-        y_pred = new_model.predict(input_data)
-        time = data.test_timestamp[run_idx][idx][0]
-        # print(time)
-        x_label = data.test_labels[run_idx][idx][0]
-        # print(input_data)
-        # print(time)
-        # print(y_pred)
+    for run_idx in range(data.test_data.shape[0]):
 
-        if idx != 0:
+        for idx in tqdm(range(0, 1024, window_size * 32)):
+            input_data = data.test_data[run_idx][idx:idx + window_size]
+            input_data = np.reshape(input_data, (1, window_size, 128))
+            y_pred = new_model.predict(input_data)
+            time = data.test_timestamp[run_idx][idx][0]
+            # print(time)
+            x_label = data.test_labels[run_idx][idx][0]
+            # print(input_data)
+            # print(time)
+            # print(y_pred)
 
-            # prev_x = data.test_labels[0][idx + window_size - 1:idx + window_size][0][0]
-            # y_pred[0][0] = data.test_labels[0][idx + window_size:idx + 16][0][0]
-            speed = (y_pred[0][0] - prev_x) / (time - prev_time)
-            real_speed = (x_label - prev_x_label) / (time - prev_time)
+            if idx != 0:
 
-            # print("real loc1:", x_label)
-            # print("real loc0:", prev_x_label)
-            # print("loc1:", y_pred[0][0])
-            # print("loc0:", prev_x)
-            # print("delta meter:", (y_pred[0][0] - prev_x), "delta seconds:",
-            #       (time - prev_time))
-            # print("Speed:", speed)
-            # print("Real Speed:", real_speed)
-            # print("---------------------------------------------------------")
-            speeds.append(speed)
-            real_speeds.append(real_speed)
+                # prev_x = data.test_labels[0][idx + window_size - 1:idx + window_size][0][0]
+                # y_pred[0][0] = data.test_labels[0][idx + window_size:idx + 16][0][0]
+                speed = abs(y_pred[0][0] - prev_x) / abs(time - prev_time)
+                real_speed = abs(x_label - prev_x_label) / abs(time -
+                                                               prev_time)
 
-        prev_x = y_pred[0][0]
-        prev_x_label = x_label
-        prev_time = time
+                # print("real loc1:", x_label)
+                # print("real loc0:", prev_x_label)
+                # print("loc1:", y_pred[0][0])
+                # print("loc0:", prev_x)
+                # print("delta meter:", (y_pred[0][0] - prev_x),
+                #       "delta seconds:", (time - prev_time))
+                # print("Speed:", speed)
+                # print("Real Speed:", real_speed)
+                # print(
+                #     "---------------------------------------------------------"
+                # )
+                speeds.append(speed)
+                real_speeds.append(real_speed)
+
+            prev_x = y_pred[0][0]
+            prev_x_label = x_label
+            prev_time = time
 
     plt.plot(speeds, label="Predicted Speed")
     plt.plot(real_speeds, label="Real Speed")
-    plt.ylim((0, 7))
+    plt.ylim((0, 70))
     plt.xlabel("Window")
     plt.ylabel("Speed (m/s)")
     MSE = np.square(np.subtract(real_speeds, speeds)).mean()
-    plt.text(0, 6, f"MSE: {MSE:.2f}")
-    plt.title("Estimated vs Real speed per sensor reading window")
+    plt.text(0, 60, f"MSE: {MSE:.2f}")
+    plt.title(
+        f"Estimated vs Real speed per sensor reading window, run: {run_idx}")
     plt.legend()
     plt.show()
 
