0195d63591e22687fc72efae7871f9a6d6aa7ecd
Brandsma <a.brandsma@protonmail.com>
2023-02-08 15:39:21 +0100

    translations

--- src/translation_key.py
+++ src/translation_key.py
@@ -6,6 +6,12 @@ translation_key = {
     "mult_path": "Saw Path",
     "offset_inverse": "Inverse Offset",
     "sine": "Sine",
+    "low_noise_parallel": "Low Noise Parallel",
+    "medium_noise_parallel": "Medium Noise Parallel",
+    "high_noise_parallel": "High Noise Parallel",
+    "low_noise_saw": "Low Noise Saw",
+    "medium_noise_saw": "Medium Noise Saw",
+    "high_noise_saw": "High Noise Saw",
 }
 
 model_key = {
